f330ffbd61f94e052c02ba6e71d1bad723ce0549
anhtqweb <tranquanganh99@gmail.com>
2021-03-24 15:51:15 +0700

    Made changes to the visuals of the interface

--- application.py
+++ application.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify,json
 
 app = Flask(__name__)
 
+# @app.route('/hello', methods=['GET', 'POST'])
+# def hello():
+
+#     # POST request
+#     if request.method == 'POST':
+#         print('Incoming..')
+#         print(request.get_json())  # parse as JSON
+#         return 'OK', 200
+
+#     # GET request
+#     else:
+#         message = {'greeting':'Hello from Flask!'}
+#         return jsonify(message)  # serialize and use JSON headers
+
+@app.route("/message", methods=["GET", "POST"])
+def message():
+    if request.method == "POST":
+        print(request.get_json()["message"])
+        return "OK", 200
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    events = [{"name": "exercise", "day": "Mon", "time":"M"}, {"name":"appointment", "day":"Thu", "time":"N"},
+                {"name":"work", "day":"Sat","time":"A"}]
+    return render_template("index.html", events=events)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/medication.js
+++ static/js/medication.js
@@ -1,190 +1,142 @@
 var my_hand = {"count": 0, "type": "none"};
-var medicine_count = {"red": 10, "blue": 10};
-var calendar_count = {"Mon1r": 0, "Mon2r": 0, "Mon3r": 0, "Mon4r": 0, "Mon1b": 0, "Mon2b": 0, "Mon3b": 0, "Mon4b": 0, "Tues1r": 0, "Tues2r": 0, "Tues3r": 0, "Tues4r": 0, "Tues1b": 0, "Tues2b": 0, "Tues3b": 0, "Tues4b": 0, "Wed1r": 0, "Wed2r": 0, "Wed3r": 0, "Wed4r": 0, "Wed1b": 0, "Wed2b": 0, "Wed3b": 0, "Wed4b": 0, "Thurs1r": 0, "Thurs2r": 0, "Thurs3r": 0, "Thurs4r": 0, "Thurs1b": 0, "Thurs2b": 0, "Thurs3b": 0, "Thurs4b": 0, "Fri1r": 0, "Fri2r": 0, "Fri3r": 0, "Fri4r": 0, "Fri1b": 0, "Fri2b": 0, "Fri3b": 0, "Fri4b": 0, "Sat1r": 0, "Sat2r": 0, "Sat3r": 0, "Sat4r": 0, "Sat1b": 0, "Sat2b": 0, "Sat3b": 0, "Sat4b": 0, "Sun1r": 0, "Sun2r": 0, "Sun3r": 0, "Sun4r": 0, "Sun1b": 0, "Sun2b": 0, "Sun3b": 0, "Sun4b": 0}
-var activities = [{"name": "exercise", "day": "Mon", "time":"1"}, {"name":"appointment", "day":"Thurs", "time":"2"},
-                {"name":"work", "day":"Sat","time":"3"}];
+var medicine_count = {"red": 11, "blue": 11};
+var calendar_count = {"Mon-M": 0, "Mon-N": 0, "Mon-A": 0, "Mon-E": 0, "Tue-M": 0, "Tue-N": 0, "Tue-A": 0, "Tue-E": 0,
+"Wed-M": 0, "Wed-N": 0, "Wed-A": 0, "Wed-E": 0, "Thu-M": 0, "Thu-N": 0, "Thu-A": 0, "Thu-E": 0, 
+"Fri-M": 0, "Fri-N": 0, "Fri-A": 0, "Fri-E": 0, "Sat-M": 0, "Sat-N": 0, "Sat-A": 0, "Sat-E": 0, 
+"Sun-M": 0, "Sun-N": 0, "Sun-A": 0, "Sun-E": 0};
+const time_mapping = {"M":"morning", "N":"noon", "A":"afternoon", "E":"evening"};
 
-function populate_activity(activity) {
-    var div = document.getElementById(activity.day + activity.time);
+function populate_events(event) {
+    var div = document.getElementById(event.day + "-" + event.time);
     while (div.lastChild) {
         div.removeChild(div.lastChild);
     }
-    div.innerHTML = activity.name;
-}
-activities.forEach(populate_activity);
-
-function create_pill(color) {
-    console.log("start");
-    var img = document.createElement("img");
-    if (color == "r") {
-        img.src = "/static/img/redpill.jpg";
-    } else {
-        img.src = "/static/img/bluepill.jpg";
-    }
-    console.log(img.src);
-    return img;
+    var span = document.createElement("span");
+    span.innerHTML = event.name;
+    div.appendChild(span);
 }
 
-function calender_click(clicked_id)
-    {
-        var calendar_val = Number(document.getElementById(clicked_id).innerHTML);
-        var calendar_type = clicked_id.substr(-1);
+// function create_pill(color) {
+//     var pill = document.createElement("div");
+//     pill.setAttribute("onClick", "pill_click()");
+//     if (color == "r") {
+//         pill.classList.add("red_pill");
+//     } else {
+//         pill.classList.add("blue_pill");
+//     }
+//     return pill;
+// }
+
+function send_action(action) {
+    $.ajax({
+        type: "POST",
+        url: "/message",
+        contentType: "application/json;charset=UTF-8",
+        data: JSON.stringify({"message": action}),
+        dataType: "json"
+    });
+}
+
+function calendar_click(clicked_id) {
         var hand_val = my_hand["count"];
         var hand_type = my_hand["type"];
 
-        if (hand_val == 1){
-            if (hand_type == calendar_type && calendar_type == 'r'){
-                var id = clicked_id + calendar_count[clicked_id ]
-                calendar_count[clicked_id ] += 1;
+        if (hand_val == 1) {
+            // var id = clicked_id + calendar_count[clicked_id ]
+            calendar_count[clicked_id ] += 1;
 
-                var img = create_pill("r");
-                img.id = id;
-                img.setAttribute("height" , 30);
-                document.getElementById(clicked_id).appendChild(img);
-                document.getElementById("pill").firstChild.remove();
-                // document.getElementById("hand").innerHTML = "none";
+            var pill = document.getElementById("pill").firstChild;
+            document.getElementById(clicked_id).appendChild(pill);
+            // $("#pill:first-child").appendTo($("#"+clicked_id));
+            // $("#"+clicked_id).append($("#pill:first-child"));
 
-                my_hand["count"] = 0;
-                my_hand["type"] = "none";
-            }
-            else if (hand_type == "b" && hand_type == calendar_type){
-                var id = clicked_id + calendar_count[clicked_id ] 
-                calendar_count[clicked_id ] += 1;
-
-                var img = create_pill("b");
-                img.id = id;
-                img.setAttribute("height" , 30);
-                document.getElementById(clicked_id).appendChild(img);
-                document.getElementById("pill").firstChild.remove();
-                // document.getElementById("hand").innerHTML = "none";
-                my_hand["count"] = 0;
-                my_hand["type"] = "none"
-            }
-            else{
-                alert("You are butting the pill in a wrong container!!");
-            }
-            
+            my_hand["count"] = 0;
+            // my_hand["type"] = "none";
+            send_action("add pill " + hand_type + " " + clicked_id.substring(0,3) + " " + time_mapping[clicked_id.slice(-1)]);    // Change clicked id ??? seperate day and time
         }
-        else if(hand_val == 0){
-            if (calendar_count[clicked_id] > 0){
-                var id = clicked_id + String(calendar_count[clicked_id] - 1);
-                document.getElementById(id).remove();
-                calendar_count[clicked_id] -= 1;
-
-                var img = create_pill(calendar_type);
-                img.setAttribute("height", 42);
-                document.getElementById("pill").appendChild(img);
-                // document.getElementById("hand").innerHTML = calendar_type;
-                my_hand["count"] = 1;
-                my_hand["type"] = calendar_type;
-            }
-            else{
-                alert("The hand is empty!");
-            }           
+        else {
+            send_action("empty hand");
         }
         
     }
 
 function red_instruction(){
-    alert("Red pill instructions!");
+    send_action("red-instruction");
 }
 
 function blue_instruction(){
-    alert("Blue pill instructions!");
+    send_action("blue-instruction");
 }
 
-function blue_medicine_click(){
+function pill_click(clicked_id) {
     var hand_val = my_hand["count"];
-    var hand_type = my_hand["type"];
+    var color = clicked_id.match(/[A-Za-z]+/g);
+    var pill = $("#"+clicked_id);
+    console.log(pill);
 
-    if (hand_val == 1 && hand_type == "r"){
-        alert("You can't put a red pill inside a blue bottle!");
+    if (hand_val == 1) {
+        send_action("hand is full");
     }
-    else if (hand_val == 1 && hand_type == "b"){
-        medicine_count["blue"] += 1;
-        var id = "blue" + String(medicine_count["blue"]);
-
-        var img = create_pill("b");
-        img.id = id;
-        img.setAttribute("height", "42");
-        // img.setAttribute("width", "42");
-
-        document.getElementById("blue_bottle").appendChild(img);
-        document.getElementById("pill").firstChild.remove();
-        // document.getElementById(id).style.backgroundColor = "blue";
-        // document.getElementById("hand").innerHTML = "none";
-        my_hand["count"] = 0;
-        my_hand["type"] = "none"
-    }
-    else if (hand_val == 0){
-        if (medicine_count["blue"] > 0) {
-            var id = "blue" + String(medicine_count["blue"]);
-            
-            var img = create_pill("b");
-            img.id = id;
-            img.setAttribute("height", "42");
-            document.getElementById(id).remove();
-            document.getElementById("pill").appendChild(img);
-            // document.getElementById("hand").innerHTML = hand_val + 1;
-            // document.getElementById("hand").innerHTML = "b";
-            medicine_count["blue"] -= 1;
-            my_hand["count"] = 1
-            my_hand["type"] = "b"
+    else {
+        if (pill.parent().parent().attr("id") == "pill-area") {
+            medicine_count[color] -= 1;
+            send_action("grab " + color + " pill from bottle");
         }
-        else{
-            alert("Red medicine bottle is empty");
-        } 
+        else if (pill.parent().parent().attr("id") == "grid-container") {
+            var calId = pill.parent().attr("id");
+            calendar_count[calId] -= 1;
+            send_action("remove " + color + " pill from calendar");
+        }
+        pill.appendTo("#pill");
+        my_hand["count"] = 1;
+        my_hand["type"] = color;
     }
-    else{
-        alert("Wrong operation!");
+}
+
+function blue_medicine_click() {
+    var hand_val = my_hand["count"];
+    if (medicine_count["blue"] == 0) {
+        send_action("out of blue pill");
+    }
+    else if (hand_val == 1) {
+    
+        var pill = document.getElementById("pill").firstChild;
+        var color = pill.id.match(/[A-Za-z]+/g);
+        if (color == "blue") {
+            document.getElementById("blue_bottle").appendChild(pill);
+            my_hand["count"] = 0;
+            send_action("return blue pill to bottle");
+        }
+        else {
+            send_action("return blue pill to wrong bottle");
+        }
+    }
+    else {
+        send_action("no pill to return");
     }
 }
 
 
 function red_medicine_click(){
     var hand_val = my_hand["count"];
-    var hand_type = my_hand["type"];
-
-    // document.body.style.cursor = "url(redpill.jpg), auto";
+    if (medicine_count["red"] == 0) {
+        send_action("out of red pill");
+    }
+    else if (hand_val == 1) {
     
-    if (hand_val == 1 && hand_type == "b"){
-        alert("You can't put a blue pill inside a red bottle!");
-    }
-    else if (hand_val == 1 && hand_type == "r"){
-        medicine_count["red"] += 1;
-        var id = "red" + String(medicine_count["red"]);
-
-        var img = create_pill("r");
-        img.id = id;
-        img.setAttribute("height" , 42);
-
-        document.getElementById("red_bottle").appendChild(img);
-        document.getElementById("pill").firstChild.remove();
-        // document.getElementById("hand").innerHTML = hand_val - 1;
-        // document.getElementById("hand").innerHTML = "none";
-        my_hand["count"] = 0;
-        my_hand["type"] = "none";
-    }
-    else if (hand_val == 0){
-        if (medicine_count["red"] > 0) {
-            var id = "red" + String(medicine_count["red"]);
-            
-            var img = create_pill("r");
-            img.id = id;
-            img.setAttribute("height" , 42);
-            document.getElementById(id).remove();
-            document.getElementById("pill").appendChild(img);
-            // document.getElementById("hand").innerHTML = "r";
-            medicine_count["red"] -= 1;
-            my_hand["count"] = 1;
-            my_hand["type"] = "r";
+        var pill = document.getElementById("pill").firstChild;
+        var color = pill.id.match(/[A-Za-z]+/g);
+        if (color == "red") {
+            document.getElementById("red_bottle").appendChild(pill);
+            my_hand["count"] = 0;
+            send_action("return pill to bottle");
         }
-        else{
-            alert("Red medicine bottle is empty");
-        } 
+        else {
+            send_action("return to wrong bottle");
+        }
     }
-    else{
-        alert("Wrong operation!");
+    else {
+        send_action("no pill to return");
     }
     
 }
