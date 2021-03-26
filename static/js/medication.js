aa2100dbdb3be7cdfde0b0bdedaf9a8e4f1a6667
anhtqweb <tranquanganh99@gmail.com>
2021-03-27 03:27:18 +0700

    Reorganized medication area and made appropriate changes to variables' names

--- static/js/medication.js
+++ static/js/medication.js
@@ -1,10 +1,12 @@
-var my_hand = {"count": 0, "type": "none"};
+var my_hand = {"count": 0, "color": "none", "name":"none"};
 var medicine_count = {"red": 11, "blue": 11};
-var calendar_count = {"Mon-M": 0, "Mon-N": 0, "Mon-A": 0, "Mon-E": 0, "Tue-M": 0, "Tue-N": 0, "Tue-A": 0, "Tue-E": 0,
-"Wed-M": 0, "Wed-N": 0, "Wed-A": 0, "Wed-E": 0, "Thu-M": 0, "Thu-N": 0, "Thu-A": 0, "Thu-E": 0, 
-"Fri-M": 0, "Fri-N": 0, "Fri-A": 0, "Fri-E": 0, "Sat-M": 0, "Sat-N": 0, "Sat-A": 0, "Sat-E": 0, 
-"Sun-M": 0, "Sun-N": 0, "Sun-A": 0, "Sun-E": 0};
-const time_mapping = {"M":"morning", "N":"noon", "A":"afternoon", "E":"evening"};
+var calendar_count = {"mon1": 0, "mon2": 0, "mon3": 0, "mon4": 0, "tue1": 0, "tue2": 0, "tue3": 0, "tue4": 0,
+"wed1": 0, "wed2": 0, "wed3": 0, "wed4": 0, "thu1": 0, "thu2": 0, "thu3": 0, "thu4": 0, 
+"fri1": 0, "fri2": 0, "fri3": 0, "fri4": 0, "sat1": 0, "sat2": 0, "sat3": 0, "sat4": 0, 
+"sun1": 0, "sun2": 0, "sun3": 0, "sun4": 0};
+const time_mapping = {"1":"morning", "2":"noon", "3":"afternoon", "4":"evening"};
+const day_mapping = {"mon":"Monday", "tue":"Tuesday", "wed":"Wednesday", "thu":"Thursday", "fri":"Friday",
+"sat":"Saturday", "sun":"Sunday"};
 
 function populate_events(event) {
     var div = document.getElementById(event.day + "-" + event.time);
@@ -16,17 +18,6 @@ function populate_events(event) {
     div.appendChild(span);
 }
 
-// function create_pill(color) {
-//     var pill = document.createElement("div");
-//     pill.setAttribute("onClick", "pill_click()");
-//     if (color == "r") {
-//         pill.classList.add("red_pill");
-//     } else {
-//         pill.classList.add("blue_pill");
-//     }
-//     return pill;
-// }
-
 function send_action(action) {
     $.ajax({
         type: "POST",
@@ -37,24 +28,25 @@ function send_action(action) {
     });
 }
 
-function calendar_click(clicked_id) {
+function calendar_click(pos_id) {
         var hand_val = my_hand["count"];
-        var hand_type = my_hand["type"];
+        var hand_color = my_hand["color"];
 
         if (hand_val == 1) {
             // var id = clicked_id + calendar_count[clicked_id ]
-            calendar_count[clicked_id ] += 1;
+            calendar_count[pos_id] += 1;
 
-            var pill = document.getElementById("pill").firstChild;
-            document.getElementById(clicked_id).appendChild(pill);
+            var med = document.getElementById("med").firstChild;
+            document.getElementById(pos_id).appendChild(med);
             // $("#pill:first-child").appendTo($("#"+clicked_id));
             // $("#"+clicked_id).append($("#pill:first-child"));
 
             my_hand["count"] = 0;
             // my_hand["type"] = "none";
-            send_action("add pill " + hand_type + " " + clicked_id.substring(0,3) + " " + time_mapping[clicked_id.slice(-1)]);    // Change clicked id ??? seperate day and time
+            send_action("(add_to_grid " + hand_color + " " + day_mapping[pos_id.substring(0,3)] + " " + time_mapping[pos_id.slice(-1)]) + ")";
         }
         else {
+            send_action("(add_to_grid " + hand_color + " " + day_mapping[pos_id.substring(0,3)] + " " + time_mapping[pos_id.slice(-1)]) + ")";
             send_action("empty hand");
         }
         
@@ -68,77 +60,57 @@ function blue_instruction(){
     send_action("blue-instruction");
 }
 
-function pill_click(clicked_id) {
+function med_click(med_id) {
     var hand_val = my_hand["count"];
-    var color = clicked_id.match(/[A-Za-z]+/g);
-    var pill = $("#"+clicked_id);
-    console.log(pill);
+    var color = med_id.match(/[A-Za-z]+/g);
+    var med = $("#"+med_id);
+    console.log(med_id);
+    // console.log(pill);
 
     if (hand_val == 1) {
+        if (med.parent().parent().attr("id") == "med-area") {
+            send_action("(remove_from_container " + color + " )");
+        } else if (med.parent().parent().attr("id") == "grid-container") {
+            var calId = med.parent().attr("id");
+            send_action("(remove_from_grid " + color + " " + day_mapping[calId.substring(0,3)] + " " + time_mapping[calId.slice(-1)]) + ")";
+        }
         send_action("hand is full");
     }
     else {
-        if (pill.parent().parent().attr("id") == "pill-area") {
+        if (med.parent().parent().attr("id") == "med-area") {
             medicine_count[color] -= 1;
-            send_action("grab " + color + " pill from bottle");
+            send_action("(remove_from_container " + color + " )");
         }
-        else if (pill.parent().parent().attr("id") == "grid-container") {
-            var calId = pill.parent().attr("id");
+        else if (med.parent().parent().attr("id") == "grid-container") {
+            var calId = med.parent().attr("id");
             calendar_count[calId] -= 1;
-            send_action("remove " + color + " pill from calendar");
+            send_action("(remove_from_grid " + color + " " + day_mapping[calId.substring(0,3)] + " " + time_mapping[calId.slice(-1)]) + ")";
         }
-        pill.appendTo("#pill");
+        med.appendTo("#med");
         my_hand["count"] = 1;
-        my_hand["type"] = color;
+        my_hand["color"] = color;
     }
 }
 
-function blue_medicine_click() {
+function container_click(container_id) {
     var hand_val = my_hand["count"];
-    if (medicine_count["blue"] == 0) {
-        send_action("out of blue pill");
-    }
-    else if (hand_val == 1) {
-    
-        var pill = document.getElementById("pill").firstChild;
-        var color = pill.id.match(/[A-Za-z]+/g);
-        if (color == "blue") {
-            document.getElementById("blue_bottle").appendChild(pill);
+    var containerColor = container_id.split("_")[0];
+    if (medicine_count[containerColor] == 0) {
+        send_action(containerColor + "container empty");
+    } else if (hand_val == 1) {
+        var med = document.getElementById("med").firstChild;
+        var medColor = med.id.match(/[A-Za-z]+/g);
+        if (medColor == containerColor) {
+            document.getElementById(containerColor + "_container").appendChild(med);
             my_hand["count"] = 0;
+            medicine_count[containerColor] += 1;
             send_action("return blue pill to bottle");
-        }
-        else {
+        } else {
             send_action("return blue pill to wrong bottle");
         }
-    }
-    else {
+    } else {
         send_action("no pill to return");
     }
-}
-
-
-function red_medicine_click(){
-    var hand_val = my_hand["count"];
-    if (medicine_count["red"] == 0) {
-        send_action("out of red pill");
-    }
-    else if (hand_val == 1) {
-    
-        var pill = document.getElementById("pill").firstChild;
-        var color = pill.id.match(/[A-Za-z]+/g);
-        if (color == "red") {
-            document.getElementById("red_bottle").appendChild(pill);
-            my_hand["count"] = 0;
-            send_action("return pill to bottle");
-        }
-        else {
-            send_action("return to wrong bottle");
-        }
-    }
-    else {
-        send_action("no pill to return");
-    }
-    
 }
 
 var json = {"day" : { "Sun": { "1": { "red" : 0, "blue" : 0 } , "2": { "red" : 0, "blue" : 0 } , "3": { "red" : 0, "blue" : 0 } , "4": { "red" : 0, "blue" : 0 } } , "Mon": { "1": { "red" : 0, "blue" : 0 } , "2": { "red" : 0, "blue" : 0 } , "3": { "red" : 0, "blue" : 0 } , "4": { "red" : 0, "blue" : 0 } } , "Tues": { "1": { "red" : 0, "blue" : 0 } , "2": { "red" : 0, "blue" : 0 } , "3": { "red" : 0, "blue" : 0 } , "4": { "red" : 0, "blue" : 0 } } , "Wed": { "1": { "red" : 0, "blue" : 0 } , "2": { "red" : 0, "blue" : 0 } , "3": { "red" : 0, "blue" : 0 } , "4": { "red" : 0, "blue" : 0 } } , "Thurs": { "1": { "red" : 0, "blue" : 0 } , "2": { "red" : 0, "blue" : 0 } , "3": { "red" : 0, "blue" : 0 } , "4": { "red" : 0, "blue" : 0 } } , "Fri": { "1": { "red" : 0, "blue" : 0 } , "2": { "red" : 0, "blue" : 0 } , "3": { "red" : 0, "blue" : 0 } , "4": { "red" : 0, "blue" : 0 } } , "Sat": { "1": { "red" : 0, "blue" : 0 } , "2": { "red" : 0, "blue" : 0 } , "3": { "red" : 0, "blue" : 0 } , "4": { "red" : 0, "blue" : 0 } } } }
